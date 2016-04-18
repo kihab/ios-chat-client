7995e3a4023e3644416d3a02707536b381c13312
Karim Ihab <karim.ihab@gmail.com>
2016-04-19 01:41:15 +0200

    some Comments for the Welcome View Controller

--- MeeDocChatClient/WelcomeViewController.swift
+++ MeeDocChatClient/WelcomeViewController.swift
@@ -14,39 +14,43 @@ class WelcomeViewController: UIViewController {
     @IBOutlet weak var WelcomLabelTopConstraint: NSLayoutConstraint!
     
     override func viewWillAppear(animated: Bool) {
-        //
+        
+        
+        //Empty textField for new user login
         self.userNameTextField.text = ""
         
-        //..
+        //Dismiss Keyboard when user taps any where is the screen
         //Looks for single or multiple taps.
         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ChatViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
         
-        //..
+        //observer on the keyboard to adjust screen layout on show/hide
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ChatViewController.keyboardWillShow(_:)), name:UIKeyboardWillShowNotification, object: nil);
         NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ChatViewController.keyboardWillHide(_:)), name:UIKeyboardWillHideNotification, object: nil);
 
 
     }
     
+    //Adjust Screen layout when Keyboard appear
     func keyboardWillShow(notification: NSNotification) {
         UIView.animateWithDuration(0.1, animations: { () -> Void in
             self.WelcomLabelTopConstraint.constant = self.WelcomLabelTopConstraint.constant - 70
         })
     }
-    
+    //Adjust Screen layout when Keyboard disappear
     func keyboardWillHide(notification: NSNotification) {
         UIView.animateWithDuration(0.1, animations: { () -> Void in
             self.WelcomLabelTopConstraint.constant = Constants.initialWelcomLabelTopConstraint
         })
     }        
     
-    //Calls this function when the tap is recognized.
+    //when the tap is recognized.
     func dismissKeyboard() {
         //Causes the view (or one of its embedded text fields) to resign the first responder status.
         view.endEditing(true)
     }    
     
+    //Make sure the user did entered his name before opening chatViewController
     override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
         
         if identifier ==  Constants.openChatSegue {
@@ -58,6 +62,7 @@ class WelcomeViewController: UIViewController {
         return true
     }
     
+    //Pass username to the chatViewController
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         if segue.identifier ==  Constants.openChatSegue {
